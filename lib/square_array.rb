01782f696d2bf28baa17575526039a339c21b711
Catherine O'Hara <catherine.ohara@outlook.com>
2019-06-19 03:43:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -14,10 +14,9 @@ end
 
 def square_array(array)
   counter = 0
-  new_array = []
   
   while counter < array.length do
-    new_array <<  array[counter]**2
+    puts array[counter]**2
     counter += 1 
   end
 end
