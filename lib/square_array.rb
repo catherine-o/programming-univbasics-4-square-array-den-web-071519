a3c2fc8f9dc535d416be88c409707bd30269090a
Catherine O'Hara <catherine.ohara@outlook.com>
2019-06-19 03:49:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,15 +1,4 @@
-def square_array(array)
-  counter = 0
-  new_array = []
-  
-  while counter < array.length do
-    new_array <<  array[counter]**2
-    counter += 1 
-  end
-  
-  new_array
-  
-end
+
 
 
 def square_array(array)
